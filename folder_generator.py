1598b001ec2adf93d2be107085b7ae7d2f75fddf
gsikora1028 <gabrielsikora28@gmail.com>
2023-07-18 10:43:32 -0700

    comments

--- folder_generator.py
+++ folder_generator.py
@@ -1,5 +1,5 @@
 # *********************************************************************************************
-# Script: Daily Folder Generator
+# Program: Daily Folder Generator
 # Author: Gabe Sikora
 # Date: 6/27/2023
 # Function: Automatically create a subfolder with the current date to store that day's files
@@ -8,20 +8,16 @@
 import os
 from datetime import datetime
 
-# Prompt user for directory and folder creation
 directory = "/Users/gsikora/Desktop/GH/July"
-create_folder = input("\nCreate a new folder? (y/n): ")
+print(f"Accessing {directory} diretory")
+current_date = datetime.now().strftime("%m.%d.%Y")
+new_folder_path = os.path.join(directory, current_date)
 
-if create_folder.lower() == "y":
-    # Get the current date and time
-    current_date = datetime.now().strftime("%m.%d.%Y")
-    new_folder_path = os.path.join(directory, current_date)
-
-    try:
-        # Create the new folder
-        os.mkdir(new_folder_path)
-        print(f"New folder '{current_date}' created successfully in '{directory}'.")
-    except FileExistsError:
-        print(f"Folder '{current_date}' already exists in '{directory}'.")
+try:
+    # Create new folder based on current date
+    os.mkdir(new_folder_path)
+    print(f"\nNew folder '{current_date}' created successfully in '{directory}'.")
+except FileExistsError:
+    print(f"\nFolder '{current_date}' already exists in '{directory}'.")
 else:
     print("No new folder created.")
