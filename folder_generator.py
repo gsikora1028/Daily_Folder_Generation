503ae759ed8c4365e35d33cba10558fda577f5be
gsikora1028 <gabrielsikora28@gmail.com>
2023-07-28 16:19:03 -0600

    function

--- folder_generator.py
+++ folder_generator.py
@@ -27,23 +27,26 @@ month_dictionary = {
     "12": "December"
 }
 
-# Check if the string is in the dictionary keys & create new monthly folder based on 1st 2 digits of date string
-if current_month in month_dictionary:
-    month = month_dictionary[current_month]
-    new_folder_path = os.path.join(base_directory, month)
-    
-    # Create the folder for the month if it doesn't exist
-    try:
-        os.mkdir(new_folder_path)
-        print(f"Folder '{month}' created successfully.")
-    except FileExistsError:
-       pass
+def create_daily_folder(base_directory, current_date, current_month, month_dictionary):
+    # Check if the string is in the dictionary keys & create new monthly folder based on 1st 2 digits of date string
+    if current_month in month_dictionary:
+        month = month_dictionary[current_month]
+        new_folder_path = os.path.join(base_directory, month)
+        
+        # Create the folder for the month if it doesn't exist
+        try:
+            os.mkdir(new_folder_path)
+            print(f"\nFolder '{month}' created successfully.")
+        except FileExistsError:
+            pass
 
-    # Create new folder based on the current date
-    try:
-        os.mkdir(os.path.join(new_folder_path, current_date))
-        print(f"New folder '{current_date}' created successfully in '{new_folder_path}'.")
-    except FileExistsError:
-        print(f"Folder '{current_date}' already exists in '{new_folder_path}'.")
-else:
-    print("The month is not found in the dictionary.")
+        # Create new folder based on the current date
+        try:
+            os.mkdir(os.path.join(new_folder_path, current_date))
+            print(f"\nNew folder '{current_date}' created successfully in '{new_folder_path}'.")
+        except FileExistsError:
+            print(f"\nFolder '{current_date}' already exists in '{new_folder_path}'.")
+    else:
+        print("\nThe month is not found in the dictionary.")
+        
+create_daily_folder(base_directory, current_date, current_month, month_dictionary)
